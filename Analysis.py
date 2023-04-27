1aab3eb6d8bde5ca46ce9efdd3256a2d89f9c782
CS_Steve <111713110+SteveProkovas@users.noreply.github.com>
2023-04-27 17:53:49 +0300

    Add files via upload

--- Analysis.py
+++ Analysis.py
@@ -1,8 +1,9 @@
-import pandas
+import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
 import time
-import datetime
-from datetime import date
+import datetime as dt
+import pandas_datareader as pdr
 
 
 def ts_timeperiod(company, start_date, end_date):
@@ -35,54 +36,37 @@ def ts_statistics(df):
         print(f"Variance: {np.var(col)}")
 
 
-def ts_plot(df):
-    plt.plot(df['date'], df[column])
+def ts_plot(df, column):
+    plt.plot(df['Date'], df[column])
     plt.title(f"{column} over time")
     plt.xlabel("Date")
     plt.ylabel(column)
     plt.show()
 
 
-# Βοηθητικές συναρτήσεις για εισόδους επιλογών - δίνονται έτοιμες
 def select_ticker():
-    """Επιλογή μετοχής"""
-    print("Επιλογή εταιρικής μετοχής")
-    ticker_list = ["MMM", "ADBE", "AAPL", "AMZN", "AMD", "AMAT", "T", "CAT", "HPE", "IBM",
-                   "MSFT"]  # λίστα με τα ticker (συντομογραφίες ονομάτων μετοχών) εταιριών του Χρημ. της Νέας Υόρκης
-    print("Tickers εταιριών:", *ticker_list, sep=', ')
-    ticker = input("Εισάγετε ένα από τα παραπάνω ticker μετοχής: ").upper()
+    print("Select a stock ticker:")
+    ticker_list = ["MMM", "ADBE", "AAPL", "AMZN", "AMD", "AMAT", "T", "CAT", "HPE", "IBM", "MSFT"]
+    print("Available tickers:", *ticker_list, sep=', ')
+    ticker = input("Enter a ticker symbol from the above list: ").upper()
     while ticker not in ticker_list:
-        ticker = input("Λάθος Ticker. Εισάγετε ένα από τα παραπάνω: ").upper()
+        ticker = input("Invalid ticker symbol. Enter a symbol from the list: ").upper()
     return ticker
-
-
-def select_ts(df):
-    """Επιλογή χρονοσειράς/στήλης από dataframe"""
-    col_list = list(df.columns)
-    print("Xρονοσειρές της μετοχής (στήλες δεδομένων)", *col_list[1:], sep=', ')
-    df_col = input("Εισάγετε χρονοσειρά (στήλη δεδομένων) από τις παραπάνω): ").capitalize()
-    while df_col not in col_list:
-        df_col = input("Λάθος χρονοσειρά/στήλη! Εισάγετε μία από τις παραπάνω: ").capitalize()
-    return df_col
-
-
-def confirm_continuation(msg):
-    cont = input(msg + "(Y/N): ")
-    while cont not in ["Y", "y", "N", "n"]:
-        cont = input("Εισάγετε (Y)es ή (N)o: ")
-    return True if cont.upper() == "Y" else False
 
 
 if __name__ == "__main__":
     ticker = select_ticker()
     start_date = input("Enter start date (YYYY-MM-DD): ")
     end_date = input("Enter end date (YYYY-MM-DD): ")
-    period1 = int(time.mktime(start_date.timetuple()))
-    period2 = int(time.mktime(end_date.timetuple()))
+
+    period1 = int(time.mktime(dt.datetime.strptime(start_date, '%Y-%m-%d').timetuple()))
+    period2 = int(time.mktime(dt.datetime.strptime(end_date, '%Y-%m-%d').timetuple()))
+
     interval = '1d'
     query_string = f'https://query1.finance.yahoo.com/v7/finance/download/{ticker}?period1={period1}&period2={period2}&interval={interval}&events=history&includeAdjustedClose=true'
     stock = pd.read_csv(query_string)
     stock.to_csv(f"{ticker}.csv")
-    stock = ts_timeperiod(ticker, start_date, end_date)
+
     ts_statistics(stock)
-    ts_plot(stock)
+    column = input("Enter a column to plot: ")
+    ts_plot(stock, column)
