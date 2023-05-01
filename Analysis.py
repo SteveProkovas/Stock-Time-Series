8a3d35a003bb84e7a943dae1872433c539e96475
MSC_Steve <111713110+SteveProkovas@users.noreply.github.com>
2023-05-01 23:04:25 +0300

    Add files via upload

--- Analysis.py
+++ Analysis.py
@@ -1,17 +1,6 @@
-import pandas as pd
-import numpy as np
 import matplotlib.pyplot as plt
-import time
-import datetime as dt
-import pandas_datareader
-
-
-def ts_timeperiod(company, start_date, end_date):
-    start = dt.datetime.strptime(start_date, '%Y-%m-%d')
-    end = dt.datetime.strptime(end_date, '%Y-%m-%d')
-    df = pdr.get_data_yahoo(company, start=start, end=end)
-    df.to_csv(company + '.csv')
-    return df
+import numpy as np
+import yfinance as yf
 
 
 def ts_statistics(df):
@@ -37,36 +26,21 @@ def ts_statistics(df):
 
 
 def ts_plot(df, column):
-    plt.plot(df['Date'], df[column])
+    plt.plot(df.index, df[column])
     plt.title(f"{column} over time")
     plt.xlabel("Date")
     plt.ylabel(column)
     plt.show()
 
 
-def select_ticker():
-    print("Select a stock ticker:")
-    ticker_list = ["MMM", "ADBE", "AAPL", "AMZN", "AMD", "AMAT", "T", "CAT", "HPE", "IBM", "MSFT"]
-    print("Available tickers:", *ticker_list, sep=', ')
-    ticker = input("Enter a ticker symbol from the above list: ").upper()
-    while ticker not in ticker_list:
-        ticker = input("Invalid ticker symbol. Enter a symbol from the list: ").upper()
-    return ticker
-
-
 if __name__ == "__main__":
-    ticker = select_ticker()
+    ticker = "AAPL"  # Replace with your desired ticker symbol
     start_date = input("Enter start date (YYYY-MM-DD): ")
     end_date = input("Enter end date (YYYY-MM-DD): ")
 
-    period1 = int(time.mktime(dt.datetime.strptime(start_date, '%Y-%m-%d').timetuple()))
-    period2 = int(time.mktime(dt.datetime.strptime(end_date, '%Y-%m-%d').timetuple()))
-
-    interval = '1d'
-    query_string = f'https://query1.finance.yahoo.com/v7/finance/download/{ticker}?period1={period1}&period2={period2}&interval={interval}&events=history&includeAdjustedClose=true'
-    stock = pd.read_csv(query_string)
-    stock.to_csv(f"{ticker}.csv")
+    stock = yf.download(ticker, start=start_date, end=end_date)
 
     ts_statistics(stock)
     column = input("Enter a column to plot: ")
     ts_plot(stock, column)
+
